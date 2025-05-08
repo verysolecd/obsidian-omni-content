b49cd01f4fb8049d5d8b8497f92428ebf97ded97
markshawn2020 <shawninjuly@gmail.com>
2025-05-08 20:56:02 +0800

    formatter

--- src/platform-adapters/wechat-adapter.ts
+++ src/platform-adapters/wechat-adapter.ts
@@ -1,12 +1,12 @@
-import {BaseContentAdapter} from "src/platform-adapters/base-content-adapter";
-import {IProcessPlugin} from "src/plugins/base-process-plugin";
-import {CodeBlocksPlugin} from "src/plugins/code-blocks-plugin";
-import {HeadingsPlugin} from "src/plugins/headings-plugin";
-import {ImagesPlugin} from "src/plugins/images-plugin";
-import {LinksPlugin} from "src/plugins/links-plugin";
-import {ListsPlugin} from "src/plugins/lists-plugin";
-import {StylesPlugin} from "src/plugins/styles-plugin";
-import {TablesPlugin} from "src/plugins/tables-plugin";
+import { BaseContentAdapter } from "src/platform-adapters/base-content-adapter";
+import { IProcessPlugin } from "src/plugins/base-process-plugin";
+import { CodeBlocksPlugin } from "src/plugins/code-blocks-plugin";
+import { HeadingsPlugin } from "src/plugins/headings-plugin";
+import { ImagesPlugin } from "src/plugins/images-plugin";
+import { LinksPlugin } from "src/plugins/links-plugin";
+import { ListsPlugin } from "src/plugins/lists-plugin";
+import { StylesPlugin } from "src/plugins/styles-plugin";
+import { TablesPlugin } from "src/plugins/tables-plugin";
 // BlockquotesPlugin导入但未使用，在注释中提到了它但目前不启用
 // import {BlockquotesPlugin} from "src/plugins/processors/blockquotes-plugin";
 
@@ -15,10 +15,17 @@ import {TablesPlugin} from "src/plugins/tables-plugin";
  * 采用插件模式设计，可以灵活添加或移除处理步骤
  */
 export class WeChatAdapter extends BaseContentAdapter {
+	protected plugins: IProcessPlugin[] = [
+		new ImagesPlugin(),
 
-	protected plugins: IProcessPlugin[] = [new ImagesPlugin(), new LinksPlugin(), new HeadingsPlugin(), new ListsPlugin(), new CodeBlocksPlugin(), new TablesPlugin(), new StylesPlugin()
+		new LinksPlugin(),
+		new HeadingsPlugin(),
+		new ListsPlugin(),
+		new CodeBlocksPlugin(),
+		new TablesPlugin(),
+		new StylesPlugin(),
 		// 目前默认不启用 BlockquotesPlugin，如需启用请取消导入注释并添加到此处
-	]
+	];
 
 	protected getAdapterName(): string {
 		return "微信公众号";
